574b44f1a5fce20019923fb6e6f486576116ec78
ArgyroTsipi <100527671+ArgyroTsipi@users.noreply.github.com>
2023-04-20 16:42:04 +0300

    Add files via upload

---
+++ data/Q1/Datagen1.py
@@ -0,0 +1,30 @@
+#data generator for questionnaire with title "My first research questionnaire"
+#Set correct questionnaire id:
+questionnaireID = "63e56236f160cada4f81f95d"
+
+import json
+from random import choice
+import string
+
+
+with open('./Blank1.json','r',encoding='UTF-8') as f:
+    json_data = f.read()
+
+data = json.loads(json_data)
+#chose first object [{},...]
+data = data[0]
+questionnaireAnswered=[]
+
+for i in range(10):
+    answers = []
+    nextquestion = "P00"
+    session =  ''.join(choice(string.ascii_letters) for i in range(4))
+    for d in data['questions']:
+        if nextquestion == d["qID"]:
+            option = choice(d['options'])
+            nextquestion = option['nextqID']
+            answers.append({"qID":d['qID'], "ans":option['optID']})
+    questionnaireAnswered.append({"questionnaireID": questionnaireID,"session": session,"answers":answers})
+
+with open('Answered1.json','w') as f:
+    json.dump(questionnaireAnswered,f,indent=4)
